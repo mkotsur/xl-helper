eb35a2d1de37e87bd172689bc21c0dc0b83f999f
Mike Kotsur <miccots@gmail.com>
2014-12-31 14:34:14 +0100

    Improved puppet script

--- demo/manifests/default.pp
+++ demo/manifests/default.pp
@@ -1,13 +1,24 @@
- exec { "update_repo" :
-    command => "/usr/bin/apt-get update"
-  }
+exec { "update_repo" :
+  command => "/usr/bin/apt-get update"
+}
 
-  package { "python-pip":
-    ensure => "installed",
-   require => Exec ['update_repo'],
-  }
+exec {"disable_firewall":
+  path => ["/usr/bin/","/usr/sbin/","/bin"],
+  command => "sudo ufw disable",
+  user => root
+}
 
-  package { "vim":
-    ensure => "installed",
-   require => Exec ['update_repo'],
-  }
+package { "python-pip":
+  ensure => "installed",
+  require => Exec ['update_repo'],
+}
+
+package { "default-jre":
+  ensure => "installed",
+  require => Exec ['update_repo'],
+}
+
+package { "vim":
+  ensure => "installed",
+  require => Exec ['update_repo'],
+}
