1643585b025ff0d0f5fb91e9b7f04459daa1a086
Mike Kotsur <miccots@gmail.com>
2014-12-31 14:38:07 +0100

    version='1.0.5'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xl-helper',
-    version='1.0.5-SNAPSHOT',
+    version='1.0.5',
     description='XL Deploy helper',
     long_description='This tool helps with installation and upgrade of XL Deploy and plugins',
     author='Mike Kotsur',
